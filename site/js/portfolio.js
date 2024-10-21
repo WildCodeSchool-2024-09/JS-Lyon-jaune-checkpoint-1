58100e606b25ca22ba2f995a52cfdb921a8a2e58
wildmatthieu <matthieu.lopez@wildcodeschool.com>
2024-10-21 16:04:39 +0200

    css/html/js step 2 ok !

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,16 @@ const imgAvatar = document.querySelector("section img");
 imgAvatar.addEventListener("click", () => {
   imgAvatar.src = "image/avatar.svg";
 });
+
+const buttonChangeNameAndColor = document.querySelector(
+  "article section.description button"
+);
+
+buttonChangeNameAndColor.addEventListener("click", () => {
+  const name = document.getElementById("firstname");
+
+  let firstname = prompt("Hey ! envoie ton prénom !");
+
+  name.style.color = "white";
+  name.innerText = firstname;
+});
