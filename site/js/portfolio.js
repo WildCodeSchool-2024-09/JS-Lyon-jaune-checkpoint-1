c458eb5fe34fa60045c8072f232f444dbb0028d5
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-10-21 12:30:11 +0200

    Ajout bouton + changment prénom et couleur du texte

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,6 +1,19 @@
 const avatar = document.querySelector("#avatar");
 
 avatar.addEventListener("click", () => {
-  // Changer la source de l'image au clic
   avatar.src = "image/avatar.svg";
+});
+
+const button1 = document.querySelector("#button1");
+
+button1.addEventListener("click", () => {
+  const myFirstName = prompt("Veuillez entrer votre prénom");
+  const prénom = document.querySelector("#firstname");
+
+  if (myFirstName) {
+    prénom.textContent = myFirstName; // Change le texte du paragraphe
+    prénom.style.color = "white"; // Change la couleur du texte en blanc
+  } else {
+    alert("Aucun prénom saisi");
+  }
 });
