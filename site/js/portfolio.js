ff79016892b33e94d236268279c01f6bdbdce9eb
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2024-10-21 11:25:19 +0200

    finish step 2 in js part

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,12 @@ const avatar = document.getElementById("avatar")
 avatar.addEventListener("click", ()=>{
     avatar.src = "https://www.parcanimalierlabarben.com/wp-content/uploads/2023/07/fiche_loutre_carre.jpg"
 })
+
+const nameSpan = document.getElementById("firstname")
+const buttonName = document.getElementById("change-name-and-color")
+
+buttonName.addEventListener("click", ()=>{
+    const namePrompt = prompt("Your name :")
+    nameSpan.innerText = namePrompt
+    nameSpan.style.color = "white"
+})
