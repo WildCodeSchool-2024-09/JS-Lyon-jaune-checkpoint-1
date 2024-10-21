fc905e3e17459c504153a1e181ad0f2070023b17
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2024-10-21 11:13:30 +0200

    finish step 1 in js part

---
+++ site/js/portfolio.js
@@ -0,0 +1,5 @@
+const avatar = document.getElementById("avatar")
+
+avatar.addEventListener("click", ()=>{
+    avatar.src = "https://www.parcanimalierlabarben.com/wp-content/uploads/2023/07/fiche_loutre_carre.jpg"
+})
