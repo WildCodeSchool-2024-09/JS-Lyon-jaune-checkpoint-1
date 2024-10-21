e6c2d12a9d8785228114627adc14e07bddc1657f
wildmatthieu <matthieu.lopez@wildcodeschool.com>
2024-10-21 15:48:54 +0200

    algo step3 done !

--- algo/3/findSeniors.js
+++ algo/3/findSeniors.js
@@ -25,8 +25,35 @@ const persons = [
 
 */
 
+// Voici un exemple de tableau de personnes. Écris une fonction qui à partir d'un tableau similaire reçu en paramètre renverra un nouveau tableau,
+// lui-même contenant deux sous-tableaux :
+// - Le premier contient uniquement des devs web seniors
+// - Le second ne contient que des data analysts seniors
+// (Étant donné qu'une personne est senior si elle a 5 ans d'expérience ou plus)
+
 function findSeniors(persons) {
-  // Your code here !
+  // initialisation des données dont on va avoir besoin
+  const result = [];
+
+  const devWebSenior = [];
+  const dataAnalystSenior = [];
+
+  // coeur de l'algo / traitement de l'algo
+  persons.forEach((person) => {
+    if (person.job === "web dev" && person.experience >= 5) {
+      devWebSenior.push(person);
+    } else if (person.job === "data analyst" && person.experience >= 5) {
+      dataAnalystSenior.push(person);
+    }
+  });
+
+  // result.push(devWebSenior);
+  // result.push(dataAnalystSenior);
+
+  // renvoie vers l'exterieur du resultat attendu
+  // return result;
+
+  return [devWebSenior, dataAnalystSenior];
 }
 
 module.exports = findSeniors;
